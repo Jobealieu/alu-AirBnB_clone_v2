6a3dd9519090e8716193ffd3beb9e1935ed89314
Jobealieu <a.jobe@alustudent.com>
2026-02-08 05:21:37 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-"""This is the review class"""
-import models
+""" Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Review(BaseModel, Base):
-    """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-    """
+    """Review class to store review information"""
+
     __tablename__ = "reviews"
 
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        user = relationship("User", back_populates="reviews")
+        place = relationship("Place", back_populates="reviews")
+    else:
+        # for file storage
+        place_id = ""  # it will be the Place.id
+        user_id = ""  # it will be the User.id
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """instantiate a review object"""
+        super().__init__(*args, **kwargs)
