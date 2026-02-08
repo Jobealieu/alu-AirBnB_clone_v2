aad054af3a65d0ef359d1c1a60c0ed3d7ec7a65c
Jobealieu <a.jobe@alustudent.com>
2026-02-08 05:20:48 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-"""This is the city class"""
-import models
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import String, ForeignKey, Column
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    # initialize class for file/db storage type
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', cascade='all, delete', backref='cities')
+    """City class that will be used to represent a city"""
+
+    __tablename__ = "cities"
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship(
+            "Place", back_populates="cities", cascade="all, delete-orphan"
+        )
+        state = relationship("State", back_populates="cities")
+    else:
+        # for file storage
+        state_id = ""  # it will be the state.id
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a City Instance"""
+        super().__init__(*args, **kwargs)
