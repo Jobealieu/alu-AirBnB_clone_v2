a397210c0e02adfc31591a11dfc07c5a15b9767d
Jobealieu <a.jobe@alustudent.com>
2026-02-08 06:19:17 +0200

    Update console.py

--- console.py
+++ console.py
@@ -41,7 +41,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, line):
-        """Usage: create <class> <key 1>=<value 1> <key 2>=<value 2> ...
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
         Create a new class instance with given keys/values and print its id.
         """
         try:
@@ -51,9 +51,7 @@ class HBNBCommand(cmd.Cmd):
 
             kwargs = {}
             for i in range(1, len(my_list)):
-                if "=" not in my_list[i]:
-                    continue
-                key, value = tuple(my_list[i].split("=", 1))
+                key, value = tuple(my_list[i].split("="))
                 if value[0] == '"':
                     value = value.strip('"').replace("_", " ")
                 else:
@@ -67,10 +65,9 @@ class HBNBCommand(cmd.Cmd):
                 obj = eval(my_list[0])()
             else:
                 obj = eval(my_list[0])(**kwargs)
-            
-            # Save the object first to generate timestamps and ID
-            obj.save()
+                storage.new(obj)
             print(obj.id)
+            obj.save()
 
         except SyntaxError:
             print("** class name missing **")
@@ -190,7 +187,7 @@ class HBNBCommand(cmd.Cmd):
                 v.__dict__[my_list[2]] = eval(my_list[3])
             except Exception:
                 v.__dict__[my_list[2]] = my_list[3]
-            v.save()
+                v.save()
         except SyntaxError:
             print("** class name missing **")
         except NameError:
