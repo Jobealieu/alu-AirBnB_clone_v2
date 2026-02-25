6206e3033212052283a11cb42a7886f3295ec5f9
Jobealieu <a.jobe@alustudent.com>
2026-02-26 00:24:25 +0200

    Fix DBStorage: store scoped_session registry, close() uses remove()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -70,10 +70,8 @@ class DBStorage:
         Base.metadata.create_all(bind=self.__engine)
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = scoped_session(session_factory)
 
     def close(self):
         """Call remove on the private session attribute."""
         self.__session.remove()
-        self.reload()
